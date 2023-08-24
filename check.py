3c7c3441e39e78fff686726d0e335cfe01e43d7a
oudsmane <142983972+oudsmane@users.noreply.github.com>
2023-08-24 11:25:58 +0100

    check.py

---
+++ check.py
@@ -0,0 +1,6 @@
+game = input("Welcome to the Guess the Number game!:")
+print(f"I'm thinking of a number between 1 and 100. Can you guess what it is?:")
+x=int(input("enter your guess: 50:"))
+y=int(input("Your guess is too high. Guess again.:"))
+print(f"Enter your guess: 25 {x+y}")
+
